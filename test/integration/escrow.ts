d586ff0dc0a5670ab224ada91d4a56a308ad057a
Friedger Müffke <mail@friedger.de>
2020-04-17 00:09:10 +0200

    improve naming for escrow

--- test/integration/escrow.ts
+++ test/integration/escrow.ts
@@ -5,7 +5,6 @@ import {
   makeContractCall,
   TransactionVersion,
   FungibleConditionCode,
-  standardPrincipalCV,
   uintCV,
 } from "@blockstack/stacks-transactions";
 import {
@@ -17,22 +16,22 @@ const STACKS_API_URL = "http://127.0.0.1:9000/v2/transactions";
 
 describe("escrow contract test suite", async () => {
   it("should deposit and payout balance", async () => {
-    let keys = JSON.parse(fs.readFileSync("./keys.json").toString());
-    let keys2 = JSON.parse(fs.readFileSync("./keys2.json").toString());
+    const keysBuyer = JSON.parse(fs.readFileSync("./keys.json").toString());
+    const keysSeller = JSON.parse(fs.readFileSync("./keys2.json").toString());
 
-    var contractAddress = keys.stacksAddress;
-    let contractName = "escrow";
-    let code = fs.readFileSync("./contracts/tokens/escrow.clar").toString();
+    const contractAddress = keysBuyer.stacksAddress;
+    const contractName = "escrow";
+    const code = fs.readFileSync("./contracts/tokens/escrow.clar").toString();
 
-    let feeRate = new BigNum(3000);
-    let secretKey = keys.secretKey;
-    let secretKey2 = keys2.secretKey;
+    var feeRate = new BigNum(1280);
+    const secretKeyBuyer = keysBuyer.secretKey;
+    const secretKeySeller = keysSeller.secretKey;
 
-    let transaction = makeSmartContractDeploy(
+    var transaction = makeSmartContractDeploy(
       contractName,
       code,
       feeRate,
-      secretKey,
+      secretKeyBuyer,
       {
         nonce: new BigNum(0),
         version: TransactionVersion.Testnet,
@@ -44,64 +43,58 @@ describe("escrow contract test suite", async () => {
 
     await new Promise((r) => setTimeout(r, 10000));
 
+    console.log("deposit");
+    feeRate = new BigNum(256);
+
     transaction = makeContractCall(
       contractAddress,
       contractName,
       "deposit",
-      [uintCV(10)],
+      [uintCV(62976)],
       feeRate,
-      secretKey,
+      secretKeyBuyer,
       {
         nonce: new BigNum(1),
         version: TransactionVersion.Testnet,
         postConditions: [
           makeStandardSTXPostCondition(
-            keys.stacksAddress,
+            keysBuyer.stacksAddress,
             FungibleConditionCode.Equal,
-            new BigNum(10)
+            new BigNum(62976)
           ),
         ],
       }
     );
-
-    console.log("deposit");
     var result = await transaction.broadcast(STACKS_API_URL);
     console.log(result);
 
+    console.log("accept seller");
     transaction = makeContractCall(
       contractAddress,
       contractName,
       "accept",
       [],
       feeRate,
-      secretKey2,
+      secretKeySeller,
       {
         nonce: new BigNum(0),
         version: TransactionVersion.Testnet,
-        postConditions: [
-          makeContractSTXPostCondition(
-            contractAddress,
-            "escrow",
-            FungibleConditionCode.LessEqual,
-            new BigNum(10)
-          ),
-        ],
       }
     );
 
-    console.log("accept seller");
     var result = await transaction.broadcast(STACKS_API_URL);
     console.log(result);
 
     await new Promise((r) => setTimeout(r, 10000));
 
+    console.log("accept buyer");
     transaction = makeContractCall(
       contractAddress,
       contractName,
       "accept",
       [],
       feeRate,
-      secretKey,
+      secretKeyBuyer,
       {
         nonce: new BigNum(2),
         version: TransactionVersion.Testnet,
@@ -110,12 +103,11 @@ describe("escrow contract test suite", async () => {
             contractAddress,
             "escrow",
             FungibleConditionCode.LessEqual,
-            new BigNum(10)
+            new BigNum(62976)
           ),
         ],
       }
     );
-    console.log("accept buyer");
     result = await transaction.broadcast(STACKS_API_URL);
     console.log(result);
   });
