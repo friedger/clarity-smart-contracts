b425924c56d53876ef746a07c699df1b65d37606
Friedger Müffke <mail@friedger.de>
2020-05-26 18:17:49 +0200

    add list contract

---
+++ contracts/experiments/lists.clar
@@ -0,0 +1,3 @@
+(define-public (append-item (user-list (list 10 uint)) (item uint))
+  (ok (append user-list item))
+)
---
+++ test/unit/lists.ts
@@ -0,0 +1,77 @@
+import { assert } from "chai";
+import {
+  Provider,
+  ProviderRegistry,
+  Client,
+  Receipt,
+} from "@blockstack/clarity";
+
+class ListProvider extends Client {
+  constructor(provider: Provider) {
+    super(
+      "ST398K1WZTBVY6FE2YEHM6HP20VSNVSSPJTW0D53M.list",
+      "experiments/lists.clar",
+      provider
+    );
+  }
+
+  async append(): Promise<Receipt> {
+    const tx = this.createTransaction({
+      method: { name: "append-item", args: ["(list u10 u9)", "u8"] },
+    });
+    await tx.sign("ST398K1WZTBVY6FE2YEHM6HP20VSNVSSPJTW0D53M");
+    return await this.submitTransaction(tx);
+  }
+
+  async appendReadOnly(): Promise<Receipt> {
+    const tx = this.createQuery({
+      method: { name: "append-item", args: ["(list u10 u9)", "u8"] },
+    });
+    return await this.submitQuery(tx);
+  }
+}
+
+describe("list contract test suite", () => {
+  let provider: Provider;
+  let client: ListProvider;
+
+  describe("syntax tests", () => {
+    before(async () => {
+      provider = await ProviderRegistry.createProvider();
+      client = new ListProvider(provider);
+    });
+
+    it("should have a valid syntax", async () => {
+      await client.checkContract();
+      await client.deployContract();
+    });
+
+    after(async () => {
+      await provider.close();
+    });
+  });
+
+  describe("basic tests", () => {
+    beforeEach(async () => {
+      provider = await ProviderRegistry.createProvider();
+      client = new ListProvider(provider);
+      await client.deployContract();
+    });
+
+    it("should append", async () => {
+      const result = await client.append();
+      console.log(result);
+      assert(result.success, "but failed");
+    });
+
+    it("should append", async () => {
+      const result = await client.appendReadOnly();
+      console.log(result);
+      assert(result.success, "but failed");
+    });
+
+    afterEach(async () => {
+      await provider.close();
+    });
+  });
+});
