2a14431809dc70054ab8c6178ae40c9a39e8be06
Friedger Müffke <mail@friedger.de>
2022-05-11 11:19:01 +0200

    add blocking period to owned profiles

--- owned-profiles/contracts/owned-profiles.clar
+++ owned-profiles/contracts/owned-profiles.clar
@@ -8,34 +8,53 @@
 
 (define-map user-profile principal {contract: principal, id: uint})
 (define-map profile-user {contract: principal, id: uint} principal)
+(define-map profile-blocked-period {contract: principal, id: uint} uint)
+
+;; about 1 month
+(define-constant blocking-period u4000)
 
 (define-constant err-not-authorized (err u403))
 (define-constant err-not-found (err u404))
+(define-constant err-profile-blocked (err u500))
+(define-constant err-invalid-profile (err u501))
 
 ;; @desc register a ownable profile item. Returns `(ok true)` on success
 ;; @param ownable; an NFT or similar that is ownable
 ;; @param id; identifier of the ownable
 (define-public (register (ownable <ownable-trait>) (id uint))
-    (match (contract-call? ownable get-owner id)
-        optional-owner
-            (match optional-owner
-                owner (let ((current-profile (map-get? user-profile tx-sender))
-                            (new-profile {contract: (contract-of ownable), id: id}))
-                        (asserts! (is-eq owner tx-sender) err-not-authorized)
-                        (match current-profile
-                            profile (map-delete profile-user profile)
-                            true)
-                        (map-set user-profile tx-sender new-profile)
-                        (map-set profile-user new-profile tx-sender)
-                        (ok true))
-                err-not-found )
-        error err-not-found))
+    (let ((owner (unwrap! (unwrap! (contract-call? ownable get-owner id) err-not-found) err-not-found))
+          (current-profile (map-get? user-profile tx-sender))
+          (new-profile {contract: (contract-of ownable), id: id}))
+        (asserts! (is-eq owner tx-sender) err-not-authorized)
+        (asserts! (is-profile-ready new-profile) err-profile-blocked)
+        (match current-profile
+          profile (begin
+            (delete-profile profile)
+            (block-profile profile))
+          true)
+        (map-set user-profile tx-sender new-profile)
+        (map-set profile-user new-profile tx-sender)
+        (ok true)))
 
-;; @desc remove registration entry of tx-sender's ownable profile. Returns `(ok true)` on success
+;; @desc remove profile entry of tx-sender's ownable profile.
+;; Returns `(ok true)` on success
 (define-public (delete)
-  (let ((current-profile (unwrap! (map-get? user-profile tx-sender) (ok true))))
+  (let ((profile (unwrap! (map-get? user-profile tx-sender) (ok true))))
       (map-delete user-profile tx-sender)
-      (map-delete profile-user current-profile)
+      (delete-profile profile)
+      (ok true)))
+
+;; @desc remove profile entry of tx-sender's ownable profile.
+;; The profile must be owned by sender. The profile is blocked for 4000 blocks.
+;; Returns `(ok true)` on success
+(define-public (delete-and-block (ownable <ownable-trait>))
+  (let ((profile (unwrap! (map-get? user-profile tx-sender) (ok true)))
+      (owner (unwrap! (unwrap! (contract-call? ownable get-owner (get id profile)) err-not-found) err-not-found)))
+      (asserts! (is-eq owner tx-sender) err-not-authorized)
+      (asserts! (is-eq (contract-of ownable) (get contract profile)) err-invalid-profile)
+      (map-delete user-profile tx-sender)
+      (delete-profile profile)
+      (block-profile profile)
       (ok true)))
 
 ;;
@@ -50,12 +69,12 @@
 ;; @desc returns the registered profile if it is part of the provided contract.
 ;; the profile item is owned by the user at the time of calling.
 (define-public (resolve-principal-to-profile (user principal) (ownable <ownable-trait>))
-    (let ((registered-profile (unwrap! (map-get? user-profile user) (ok none)))
-        (owner (unwrap! (unwrap! (contract-call? ownable get-owner (get id registered-profile)) (ok none)) (ok none))))
-        (asserts! (is-eq (contract-of ownable) (get contract registered-profile)) (ok none))
+    (let ((profile (unwrap! (map-get? user-profile user) (ok none)))
+        (owner (unwrap! (unwrap! (contract-call? ownable get-owner (get id profile)) (ok none)) (ok none))))
+        (asserts! (is-eq (contract-of ownable) (get contract profile)) (ok none))
         (if (is-eq owner user)
-                (ok (some registered-profile))
-                (ok none))))
+            (ok (some profile))
+            (ok none))))
 
 ;; @desc returns the owner of the provided profile item if the owner registered the item as user profile.
 (define-public (resolve-profile-to-principal (ownable <ownable-trait>) (id uint))
@@ -64,3 +83,17 @@
         (if (is-eq owner registered-owner)
             (ok (some owner))
             (ok none))))
+
+;; @desc returns true if the profile is not blocked for new registrations
+(define-read-only (is-profile-ready (profile {contract: principal, id: uint}))
+  (> block-height (unwrap! (map-get? profile-blocked-period profile) true)))
+
+(define-read-only (get-profile-blocked-until (ownable <ownable-trait>) (id uint))
+  (map-get? profile-blocked-period {contract: (contract-of ownable), id: id}))
+
+;; private function
+(define-private (delete-profile (profile {contract: principal, id: uint}))
+  (map-delete profile-user profile))
+
+(define-private (block-profile (profile {contract: principal, id: uint}))
+  (map-set profile-blocked-period profile (+ block-height blocking-period)))
--- owned-profiles/tests/owned-profiles_test.ts
+++ owned-profiles/tests/owned-profiles_test.ts
@@ -10,7 +10,6 @@ Clarinet.test({
     let block = chain.mineBlock([
       Tx.contractCall('fun-nft', 'mint', [], wallet_1.address),
     ]);
-
     block.receipts[0].result.expectOk();
 
     block = chain.mineBlock([
@@ -21,7 +20,6 @@ Clarinet.test({
         wallet_1.address
       ),
     ]);
-
     block.receipts[0].result.expectOk();
 
     let profile = chain.callReadOnlyFn(
@@ -61,7 +59,6 @@ Clarinet.test({
     let block = chain.mineBlock([
       Tx.contractCall('fun-nft', 'mint', [], wallet_1.address),
     ]);
-
     block.receipts[0].result.expectOk();
 
     block = chain.mineBlock([
@@ -107,8 +104,7 @@ Clarinet.test({
       ],
       deployer.address
     );
-
-    profileData = profile.result.expectOk().expectNone();
+    profile.result.expectOk().expectNone();
   },
 });
 
@@ -131,8 +127,7 @@ Clarinet.test({
       [types.principal(wallet_1.address)],
       deployer.address
     );
-
-    let profileData = profile.result.expectNone();
+    profile.result.expectNone();
 
     block = chain.mineBlock([
       Tx.contractCall(
@@ -163,8 +158,7 @@ Clarinet.test({
       [types.principal(wallet_1.address)],
       deployer.address
     );
-
-    profileData = profile.result.expectNone();
+    profile.result.expectNone();
 
     profile = chain.callReadOnlyFn(
       'owned-profiles',
@@ -175,7 +169,283 @@ Clarinet.test({
       ],
       deployer.address
     );
+    profile.result.expectOk().expectNone();
 
-    profileData = profile.result.expectOk().expectNone();
+    // profile is not blocked
+    const blockedUntil = chain.callReadOnlyFn(
+      'owned-profiles',
+      'get-profile-blocked-until',
+      [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+      deployer.address
+    );
+    blockedUntil.result.expectNone();
+  },
+});
+
+Clarinet.test({
+  name: 'Ensure that user can register deleted profile',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get('deployer')!;
+    let wallet_1 = accounts.get('wallet_1')!;
+    let wallet_2 = accounts.get('wallet_2')!;
+
+    let block = chain.mineBlock([
+      Tx.contractCall('fun-nft', 'mint', [], wallet_1.address),
+    ]);
+    block.receipts[0].result.expectOk();
+
+    block = chain.mineBlock([
+      // register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_1.address
+      ),
+      // delete
+      Tx.contractCall('owned-profiles', 'delete', [], wallet_1.address),
+      // transfer
+      Tx.contractCall(
+        'fun-nft',
+        'transfer',
+        [
+          types.uint(1),
+          types.principal(wallet_1.address),
+          types.principal(wallet_2.address),
+        ],
+        wallet_1.address
+      ),
+      // try to register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_2.address
+      ),
+    ]);
+    block.receipts[0].result.expectOk();
+    block.receipts[1].result.expectOk();
+    block.receipts[2].result.expectOk();
+    block.receipts[3].result.expectOk(); // profile was not blocked by previous owner
+  },
+});
+
+Clarinet.test({
+  name: 'Ensure that user can register blocked profile only after 4000 blocks',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get('deployer')!;
+    let wallet_1 = accounts.get('wallet_1')!;
+    let wallet_2 = accounts.get('wallet_2')!;
+
+    let block = chain.mineBlock([
+      Tx.contractCall('fun-nft', 'mint', [], wallet_1.address),
+    ]);
+    block.receipts[0].result.expectOk();
+
+    block = chain.mineBlock([
+      // register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_1.address
+      ),
+      // delete
+      Tx.contractCall(
+        'owned-profiles',
+        'delete-and-block',
+        [types.principal(`${deployer.address}.fun-nft`)],
+        wallet_1.address
+      ),
+      // transfer
+      Tx.contractCall(
+        'fun-nft',
+        'transfer',
+        [
+          types.uint(1),
+          types.principal(wallet_1.address),
+          types.principal(wallet_2.address),
+        ],
+        wallet_1.address
+      ),
+      // try to register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_2.address
+      ),
+    ]);
+    block.receipts[0].result.expectOk();
+    block.receipts[1].result.expectOk();
+    block.receipts[2].result.expectOk();
+    block.receipts[3].result.expectErr().expectUint(500); // profile is blocked
+
+    chain.mineEmptyBlock(3999);
+    // registration still fails
+    block = chain.mineBlock([
+      // try to register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_2.address
+      ),
+    ]);
+    block.receipts[0].result.expectErr().expectUint(500); // profile is blocked
+
+    // registration succeeds
+    block = chain.mineBlock([
+      // register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_2.address
+      ),
+    ]);
+    block.receipts[0].result.expectOk();
+  },
+});
+
+Clarinet.test({
+  name: 'Ensure that user cannot block profile after transfer',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get('deployer')!;
+    let wallet_1 = accounts.get('wallet_1')!;
+    let wallet_2 = accounts.get('wallet_2')!;
+
+    let block = chain.mineBlock([
+      Tx.contractCall('fun-nft', 'mint', [], wallet_1.address),
+    ]);
+    block.receipts[0].result.expectOk();
+
+    block = chain.mineBlock([
+      // register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_1.address
+      ),
+      // transfer
+      Tx.contractCall(
+        'fun-nft',
+        'transfer',
+        [
+          types.uint(1),
+          types.principal(wallet_1.address),
+          types.principal(wallet_2.address),
+        ],
+        wallet_1.address
+      ),
+      // try delete and block -> fails
+      Tx.contractCall(
+        'owned-profiles',
+        'delete-and-block',
+        [types.principal(`${deployer.address}.fun-nft`)],
+        wallet_1.address
+      ),
+      // delete
+      Tx.contractCall('owned-profiles', 'delete', [], wallet_1.address),
+      // register
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_2.address
+      ),
+    ]);
+    block.receipts[0].result.expectOk();
+    block.receipts[1].result.expectOk();
+    block.receipts[2].result.expectErr().expectUint(403); // profile not owned
+    block.receipts[3].result.expectOk();
+    block.receipts[4].result.expectOk();
+  },
+});
+
+Clarinet.test({
+  name: 'Ensure that user can register, delete and block in 1 block',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get('deployer')!;
+    let wallet_1 = accounts.get('wallet_1')!;
+    let wallet_2 = accounts.get('wallet_2')!;
+
+    let block = chain.mineBlock([
+      Tx.contractCall('fun-nft', 'mint', [], wallet_1.address),
+    ]);
+
+    block.receipts[0].result.expectOk();
+
+    let profile = chain.callReadOnlyFn(
+      'owned-profiles',
+      'get-unverified-profile',
+      [types.principal(wallet_1.address)],
+      deployer.address
+    );
+    profile.result.expectNone();
+
+    block = chain.mineBlock([
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_1.address
+      ),
+      Tx.contractCall(
+        'owned-profiles',
+        'delete-and-block',
+        [types.principal(`${deployer.address}.fun-nft`)],
+        wallet_1.address
+      ),
+      Tx.contractCall('owned-profiles', 'delete', [], wallet_1.address),
+    ]);
+
+    block.receipts[0].result.expectOk();
+    block.receipts[1].result.expectOk();
+    block.receipts[2].result.expectOk(); // delete is a no-operation
+
+    // profile is blocked
+    const blockedUntil = chain.callReadOnlyFn(
+      'owned-profiles',
+      'get-profile-blocked-until',
+      [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+      deployer.address
+    );
+    blockedUntil.result.expectSome().expectUint(4002);
+  },
+});
+
+Clarinet.test({
+  name: 'Ensure that user cannot register not owned profile',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let deployer = accounts.get('deployer')!;
+    let wallet_1 = accounts.get('wallet_1')!;
+    let wallet_2 = accounts.get('wallet_2')!;
+
+    let block = chain.mineBlock([
+      Tx.contractCall('fun-nft', 'mint', [], wallet_1.address),
+    ]);
+
+    block.receipts[0].result.expectOk();
+
+    let profile = chain.callReadOnlyFn(
+      'owned-profiles',
+      'get-unverified-profile',
+      [types.principal(wallet_2.address)],
+      deployer.address
+    );
+    profile.result.expectNone();
+
+    block = chain.mineBlock([
+      Tx.contractCall(
+        'owned-profiles',
+        'register',
+        [types.principal(`${deployer.address}.fun-nft`), types.uint(1)],
+        wallet_2.address
+      ),
+    ]);
+
+    block.receipts[0].result.expectErr().expectUint(403);
   },
 });
